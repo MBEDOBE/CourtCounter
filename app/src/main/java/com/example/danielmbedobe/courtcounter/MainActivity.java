0f71e9c2c227cb417ff42261fe6865d62269fdbc
MBEDOBE <danielbushbaby@gmail.com>
2018-06-01 11:21:19 +0000

    made changes to ui and java code

--- app/src/main/java/com/example/danielmbedobe/courtcounter/MainActivity.java
+++ app/src/main/java/com/example/danielmbedobe/courtcounter/MainActivity.java
@@ -1,7 +1,7 @@
 package com.example.danielmbedobe.courtcounter;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.TextView;
 
@@ -19,26 +19,32 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
     }
 
-    //increase score for team A by 3points
+    //increase score for team A by 3points for a penalty kick
     public void addThreeForTeamA(View view) {
         scoreTeamA = scoreTeamA + 3;
         displayForTeamA(scoreTeamA);
     }
 
-    //increase score for team A by 2points
+    //increase the score for team A by 3points for a drop goal
+    public void addThreePointsForTeamA(View view) {
+        scoreTeamA = scoreTeamA + 3;
+        displayForTeamA(scoreTeamA);
+    }
+
+    //increase score for team A by 2points for a conversion
     public void addTwoForTeamA(View view) {
         scoreTeamA = scoreTeamA + 2;
         displayForTeamA(scoreTeamA);
     }
 
-    //increase score for team A by 1point
+    //increase score for team A by 5points for a Try
     public void addOneForTeamA(View view) {
-        scoreTeamA = scoreTeamA + 1;
+        scoreTeamA = scoreTeamA + 5;
         displayForTeamA(scoreTeamA);
     }
 
     public void displayForTeamA(int score) {
-        TextView scoreView = (TextView) findViewById(R.id.teamA_score);
+        TextView scoreView = findViewById(R.id.teamA_score);
         scoreView.setText(String.valueOf(score));
     }
 
@@ -48,20 +54,26 @@ public class MainActivity extends AppCompatActivity {
         displayForTeamB(scoreTeamB);
     }
 
-    //increase score for team B by 2points
+    //increase the score for team A by 3points for a drop goal
+    public void addThreePointsForTeamB(View view) {
+        scoreTeamB = scoreTeamB + 3;
+        displayForTeamB(scoreTeamB);
+    }
+
+    //increase score for team B by 2points for a conversion
     public void addTwoForTeamB(View view) {
         scoreTeamB = scoreTeamB + 2;
         displayForTeamB(scoreTeamB);
     }
 
-    //increase score for team B by 1point
+    //increase score for team B by 5points for a try
     public void addOneForTeamB(View view) {
-        scoreTeamB = scoreTeamB + 1;
+        scoreTeamB = scoreTeamB + 5;
         displayForTeamB(scoreTeamB);
     }
 
     public void displayForTeamB(int score) {
-        TextView scoreView = (TextView) findViewById(R.id.teamB_score);
+        TextView scoreView = findViewById(R.id.teamB_score);
         scoreView.setText(String.valueOf(score));
     }
 
@@ -72,4 +84,6 @@ public class MainActivity extends AppCompatActivity {
         displayForTeamA(scoreTeamA);
         displayForTeamB(scoreTeamB);
     }
+
+
 }
